c2a0cd802e4eeb2413fe7af397d1651c1d2adf88
catsbi <killfine1988@gmail.com>
2021-10-08 17:02:40 +0900

    feat: 제공자 인터페이스(Supplier) 생성

---
+++ src/main/java/racinggame/supplier/DistanceSupplier.java
@@ -0,0 +1,11 @@
+package racinggame.supplier;
+
+import racinggame.domain.Distance;
+
+/**
+ * 거리 정보를 반환한다.
+ */
+@FunctionalInterface
+public interface DistanceSupplier<T extends Distance> {
+    T getDistance();
+}
---
+++ src/main/java/racinggame/supplier/NameSupplier.java
@@ -0,0 +1,11 @@
+package racinggame.supplier;
+
+import racinggame.domain.Name;
+
+/**
+ * 이름 정보를 제공한다.
+ */
+@FunctionalInterface
+public interface NameSupplier<T extends Name> {
+    T getName();
+}
